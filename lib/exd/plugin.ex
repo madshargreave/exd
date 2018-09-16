0e8e9ead444e31b0623d25d5562678473efb6c0a
Mads Hargreave <mads.hargreave@gmail.comm>
2018-09-16 21:37:57 +0200

    Added remote plugin configuration

--- lib/exd/plugin.ex
+++ lib/exd/plugin.ex
@@ -2,7 +2,22 @@ defmodule Exd.Plugin do
   @moduledoc """
   Defines helpers from both built-in and external Exd plugins
   """
+
+  # Built-in plugins
   use Exd.Plugin.String
   use Exd.Plugin.Integer
   use Exd.Plugin.Boolean
+
+  @external Application.get_env(:exd, :plugins, [])
+
+  for plugin <- @external do
+    quote do
+      use unquote(plugin)
+    end
+  end
+
+  def __helper__(expr) do
+    raise ArgumentError, message: "No plugin matches expression: #{inspect expr}"
+  end
+
 end

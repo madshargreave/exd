485968766af8bb363fbaba84c24c9e81b6d666eb
Mads Hargreave <mads.hargreave@gmail.comm>
2018-10-07 17:56:43 +0200

    stuff

--- test/exd/interpreter/plugin/api_test.exs
+++ test/exd/interpreter/plugin/api_test.exs
@@ -1,6 +1,6 @@
-defmodule Exd.Plugin.ApiTest do
-  use Exd.QueryCase
-  alias Exd.Repo
+# defmodule Exd.Plugin.ApiTest do
+#   use Exd.QueryCase
+#   alias Exd.Repo
 
 #   describe "GET /comments" do
 #     test "it returns list of comments" do
@@ -24,53 +24,53 @@ defmodule Exd.Plugin.ApiTest do
 #     end
 #   end
 
-  # describe "POST /comments" do
-  #   test "it creates a new comment" do
-  #     inserts =
-  #       from r in api_callback(:post, "/api/v1/comments"),
-  #       select: %{
-  #         text: r.params.text
-  #       },
-  #       into: database("comments"),
-  #       returning: true
+#   describe "POST /comments" do
+#     test "it creates a new comment" do
+#       inserts =
+#         from r in api_callback(:post, "/api/v1/comments"),
+#         select: %{
+#           text: r.params.text
+#         },
+#         into: database("comments"),
+#         returning: true
 
-  #     domain =
-  #       from c in inserts,
-  #       where: success?(c),
-  #       select: %{
-  #         status: 200,
-  #         data: {
-  #           id: c.id,
-  #           text: c.text,
-  #           user_id: c.user_id,
-  #           inserted_at: c.inserted_at,
-  #           updated_at: c.updated_at
-  #         }
-  #       },
-  #       into: api_response()
+#       domain =
+#         from c in inserts,
+#         where: success?(c),
+#         select: %{
+#           status: 200,
+#           data: {
+#             id: c.id,
+#             text: c.text,
+#             user_id: c.user_id,
+#             inserted_at: c.inserted_at,
+#             updated_at: c.updated_at
+#           }
+#         },
+#         into: api_response()
 
-  #     errors =
-  #       from e in inserts,
-  #       where: error?(c)
-  #       select: %{
-  #         status: status_code(e),
-  #         data: %{
-  #           type: e.type,
-  #           error: e.message
-  #         }
-  #       },
-  #       into: api_response()
+#       errors =
+#         from e in inserts,
+#         where: error?(c)
+#         select: %{
+#           status: status_code(e),
+#           data: %{
+#             type: e.type,
+#             error: e.message
+#           }
+#         },
+#         into: api_response()
 
-  #     events =
-  #       from d in domain,
-  #       select: %{
-  #         type: "comment.created",
-  #         data: d
-  #       },
-  #       into: redis_publish("domain-events")
+#       events =
+#         from d in domain,
+#         select: %{
+#           type: "comment.created",
+#           data: d
+#         },
+#         into: redis_publish("domain-events")
 
-  #     Repo.start_link([errors, events])
-  #   end
-  # end
+#       Repo.start_link([errors, events])
+#     end
+#   end
 
-end
+# end
--- test/exd/interpreter/plugin/fetch_test.exs
+++ test/exd/interpreter/plugin/fetch_test.exs
@@ -2,44 +2,6 @@ defmodule Exd.Plugin.FetchTest do
   use Exd.QueryCase
   alias Exd.Repo
 
-  # def get(symbol) do
-  #   opts = [{:symbol, key: symbol, ttl: 5000}]
-
-  #   Repo.first(
-      # from b in fetch(interpolate("https://coinmarketcap.com/currencies/?", ^symbol)),
-      # select: %{
-      #   name: html_parse_text(b.body, ".coin-name"),
-      #   symbol: html_parse_text(b.body, ".coin-symbol"),
-      #   price: html_parse_text(b.body, ".coin-price"),
-      #   marketcap: html_parse_text(b.body, ".coin-marketcap"),
-      #   volume: html_parse_text(b.body, ".coin-volume")
-      # },
-  #     opts
-  #   )
-  # end
-
-  # def perform(job) do
-  #   query =
-  #     from b in fetch("https://coinmarketcap.com"),
-  #     select: %{
-  #       name: html_parse_text(b.body, ".coin-name"),
-  #       symbol: html_parse_text(b.body, ".coin-symbol"),
-  #       price: html_parse_text(b.body, ".coin-price"),
-  #       marketcap: html_parse_text(b.body, ".coin-marketcap"),
-  #       volume: html_parse_text(b.body, ".coin-volume")
-  #     }
-
-  #   Repo.stream(
-  #     from q in subquery(query),
-  #     select: %{
-  #       query_id: ^job.query_id,
-  #       data: q
-  #     }
-  #   )
-  #   |> Stream.each(&IO.inspect/1)
-  #   |> Stream.run
-  # end
-
   describe "fetch/1" do
     test "it fetches pages" do
       assert %{
@@ -54,51 +16,5 @@ defmodule Exd.Plugin.FetchTest do
           }
         )
     end
-
-    # test "it works with piping" do
-    #   overview =
-    #     from r in fetch("https://coinmarketcap.com"),
-    #     where: r.status == 200,
-    #     select: %{
-    #       row: unnest(html_parse_list(r.body, "table#currencies > tbody > tr"))
-    #     }
-
-    #   coins =
-    #     from o in overview,
-    #     where: o.marketcap > 100_000_000,
-    #     select: %{
-    #       name: html_parse_text(o.row, ".container-name"),
-    #       href: html_parse_attr(o.row, "href", ".container-name"),
-    #       symbol: html_parse_text(o.row, ".container-symbol"),
-    #       price: html_parse_float(o.row, ".container-price"),
-    #       marketcap: html_parse_float(o.row, ".container-marketcap"),
-    #       volume: html_parse_float(o.row, ".container-volume")
-    #     }
-
-    #   detail =
-    #     from r in fetch(
-    #       interpolate("https://coinmarketcap.com/currencies/?", args.symbol),
-    #       retries: 3,
-    #       timeout: 5000
-    #     ),
-    #     where: r.status == 200,
-    #     select: %{
-    #       symbol: html_parse_text(l.body, ".container-symbol"),
-    #       explorer: html_parse_attr(l.body, "href", ".container-explorer"),
-    #       website: html_parse_attr(l.body, "href", ".container-website"),
-    #       forum: html_parse_attr(l.body, "href", ".container-forum"),
-    #       twitter_followers: html_parse_attr(l.body, "href", ".container-twitter-followers"),
-    #       reddit_subs: html_parse_attr(l.body, "href", ".container-reddit-subs")
-    #     }
-
-    #   results =
-    #     Repo.all(
-    #       from c in coins,
-    #       join: d in details, on: d.symbol = c.symbol,
-    #       where: d.twitter_followers > 1000,
-    #       select: [c, d]
-    #     )
-    # end
   end
-
 end
